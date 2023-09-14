371c7b8133faea2010dc676ef44507702ac7fdba
Ann1399 <49426999+Ann1399@users.noreply.github.com>
2023-09-14 14:39:43 -0600

    Cambios en el App e img

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import CalendarComponent from './CalendarComponent/CalendarComponent';
 import NotificationCenter from './NotificationCenter/NotificationCenter';
 import NotificationCard from './NotificationCards/NotificationCards';
+import SlideBar from './SlideBar/SlideBar';
+import Navbar from './NavbarStudents/NavbarStudents/NavbarDashboardStudents';
 
 const notification = [
   'Notificación 1',
@@ -16,29 +18,46 @@ const notification = [
 function App() {
   return (
 
-
-    <div className="MainPanel">
-
-      <div className="right-panel">
-        <RedContainer />
-
-
-        <div className="calendar-and-notifications">
-          <div className="calendar">
-            <CalendarComponent />
-          </div>
-          <div className="notifications">
-            <NotificationCenter />
-          </div>
-
-          <div className="NotificationCard">
-          <NotificationCard notifications={notification} />
-          </div>
-        </div>
-
-      </div>
-
+    <div style={{display:'flex'}}>
+    <div>
+      <SlideBar/>
     </div>
+    <div class="col-11">
+      <Navbar />
+      <div className='row' style={{display:'flex'}}>
+        <div>
+          
+        </div>
+        <div>
+        <div className="MainPanel">
+
+<div className="right-panel">
+  <RedContainer />
+
+
+  <div className="calendar-and-notifications">
+    <div className="calendar">
+      <CalendarComponent />
+    </div>
+    <div className="notifications">
+      <NotificationCenter />
+    </div>
+
+    <div className="NotificationCard">
+    <NotificationCard notifications={notification} />
+    </div>
+  </div>
+
+</div>
+
+</div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+    
 
   );
 }
